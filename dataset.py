95ba1f63a7c48455d2835d19a9238b959c88f150
ChevalierDeSangreal <415545488@qq.com>
2023-08-15 12:03:05 +0800

    Added bad words cleaner

--- dataset.py
+++ dataset.py
@@ -11,16 +11,37 @@ class TextDataset(Dataset):
 
         print("Preparing data...")
         # Load and process the data
-        
+
+        if self.args.del_bad_word:
+            with open(args.bad_word_path, 'r', encoding='utf-8') as f:
+                self.bad_words = f.readlines()
+            
+            
+        self.data = []
+        with open(args.data_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = self.clean_line(line)
+                if self.args.del_bad_word:
+                    tmp = 0
+                    for bad_word in self.bad_words:
+                        if bad_word.strip() in line:
+                            tmp = 1
+                            break
+                    if tmp:
+                        continue
+                
+                if args.model_name == 'RNN':
+                    self.data += line
+                elif args.model_name == 'GPT':
+                    if line.strip():
+                        self.data.append(line)
 
         if args.model_name == 'RNN':
-            with open(args.data_path, 'r', encoding='utf-8') as f:
-                file = f.read().lower() # Convert all letters to lowercase
-                file = re.sub(r"[^a-zA-Z0-9'\s]", '', file )
-            file_len = len(file)
-            print('Prepareing complete. got date file, length =', file_len)
+            file_len = len(self.data)
+            print('Prepareing complete. got date file, character number: ', file_len)
             # Get all unique characters in the file
-            all_characters = sorted(set(file))
+            all_characters = sorted(set(self.data))
 
             # Calculate the number of unique characters
             self.n_characters = len(all_characters)
@@ -28,28 +49,60 @@ class TextDataset(Dataset):
             self.char_to_index = {char: index for index, char in enumerate(all_characters)}
             self.index_to_char = {index: char for index, char in enumerate(all_characters)}
 
-            self.data = file
             self.chunk_len = self.args.chunk_len
             self.data_len = len(self.data) - self.args.chunk_len
+
         elif args.model_name == 'GPT':
             self.tokenizer = GPT2Tokenizer.from_pretrained(args.model_path)
             # Set the filling symbol of the word breaker as the ending symbol
             self.tokenizer.pad_token = self.tokenizer.eos_token
 
-            with open(args.data_path, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
-
             # Using clean_line each line using the line method
             # Condition line.strip() and self.clean_line(line.strip()) ensure that the line is not
             # only non empty, but also non empty after cleaning
-            self.lyrics = [self.clean_line(line.strip()) for line in lines if line.strip() and self.clean_line(line.strip())]
+            self.lyrics = self.data
             self.data_len = len(self.lyrics)
+            print(self.lyrics[:10])
+            print(self.data_len)
+
+        # if args.model_name == 'RNN':
+        #     with open(args.data_path, 'r', encoding='utf-8') as f:
+        #         file = f.read().lower() # Convert all letters to lowercase
+        #         file = re.sub(r"[^a-zA-Z0-9'\s]", '', file )
+        #     file_len = len(file)
+        #     print('Prepareing complete. got date file, length =', file_len)
+        #     # Get all unique characters in the file
+        #     all_characters = sorted(set(file))
+
+        #     # Calculate the number of unique characters
+        #     self.n_characters = len(all_characters)
+
+        #     self.char_to_index = {char: index for index, char in enumerate(all_characters)}
+        #     self.index_to_char = {index: char for index, char in enumerate(all_characters)}
+
+        #     self.data = file
+        #     self.chunk_len = self.args.chunk_len
+        #     self.data_len = len(self.data) - self.args.chunk_len
+        # elif args.model_name == 'GPT':
+        #     self.tokenizer = GPT2Tokenizer.from_pretrained(args.model_path)
+        #     # Set the filling symbol of the word breaker as the ending symbol
+        #     self.tokenizer.pad_token = self.tokenizer.eos_token
+
+        #     with open(args.data_path, 'r', encoding='utf-8') as f:
+        #         lines = f.readlines()
+
+        #     # Using clean_line each line using the line method
+        #     # Condition line.strip() and self.clean_line(line.strip()) ensure that the line is not
+        #     # only non empty, but also non empty after cleaning
+        #     self.lyrics = [self.clean_line(line.strip()) for line in lines if line.strip() and self.clean_line(line.strip())]
+        #     print(self.lyrics[:10])
+        #     self.data_len = len(self.lyrics)
 
     def clean_line(self, line):
         # Convert text to lowercase
         line = line.lower()
         # Using regular expressions to remove special characters
-        line = re.sub(r"[^a-zA-Z0-9\s]", '', line)
+        line = re.sub(r"[^a-zA-Z0-9'\s]", '', line)
         return line.strip()
     
     def __len__(self):
